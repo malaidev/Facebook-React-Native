facebook/react-native

commit 3d05bac5872b42a5f412ec9042ce5be0b5d5b8d3

Unbreak Nightly job by providing a GPG key as base64 encoded.
Summary:
It seems like CircleCI is not handling well env variables with \n in it.
I'm moving it over to a base64 encoded string and I'm extending the publish
scripts to base64 decode the key.

Changelog:
[Internal] [Changed] - Unbreak Nightly job by providing a GPG key as base64 encoded.

Reviewed By: cipolleschi

Differential Revision: D40426438

fbshipit-source-id: a60a7e7ad71580e81e675c84008d2712712e42a6

build.gradle.kts
@@ -7,6 +7,20 @@
 
 plugins { id("io.github.gradle-nexus.publish-plugin") version "1.1.0" }
 
+val reactAndroidProperties = java.util.Properties()
+
+File("./ReactAndroid/gradle.properties").inputStream().use { reactAndroidProperties.load(it) }
+
+version =
+    if (project.hasProperty("isNightly") &&
+        (project.property("isNightly") as? String).toBoolean()) {
+      "${reactAndroidProperties.getProperty("VERSION_NAME")}-SNAPSHOT"
+    } else {
+      reactAndroidProperties.getProperty("VERSION_NAME")
+    }
+
+group = "com.facebook.react"
+
 val ndkPath by extra(System.getenv("ANDROID_NDK"))
 val ndkVersion by extra(System.getenv("ANDROID_NDK_VERSION"))
 
@@ -27,7 +41,7 @@ val sonatypePassword = findProperty("SONATYPE_PASSWORD")?.toString()
 
 nexusPublishing {
   repositories {
-    create("sonatype") {
+    sonatype {
       username.set(sonatypeUsername)
       password.set(sonatypePassword)
     }

ReactAndroid/external-artifacts/.npmignore
@@ -1,0 +1,3 @@
+# Make sure we never publish the build and artifacts folders to npm.
+build/
+artifacts/

ReactAndroid/external-artifacts/build.gradle
@@ -26,6 +26,7 @@ apply from: "../publish.gradle"
 publishing {
     publications {
         getByName("release") {
+            artifactId 'react-native-artifacts'
             artifact hermesiOSArtifact
         }
     }

scripts/release-utils.js
@@ -78,6 +78,9 @@ function generateAndroidArtifacts(releaseVersion, tmpPublishingFolder) {
 
 function publishAndroidArtifactsToMaven(isNightly) {
   // -------- Publish every artifact to Maven Central
+  // The GPG key is base64 encoded on CircleCI
+  let buff = Buffer.from(env.ORG_GRADLE_PROJECT_SIGNING_KEY_ENCODED, 'base64');
+  env.ORG_GRADLE_PROJECT_SIGNING_KEY = buff.toString('ascii');
   if (exec('./gradlew publishAllToSonatype -PisNightly=' + isNightly).code) {
     echo('Failed to publish artifacts to Sonatype (Maven Central)');
     exit(1);

malaidev/Facebook-React-Native

commit 00b543220ae5c561878f8d8b7800928b6e38e73f

Fix ReactAndroid not found in rn-tester build (#35058)
Summary:
I tried to build the source locally to test changes with `rn-tester`. However, the gradle build failed due to "./ReactAndroid/gradle.properties (No such file or directory)". I believe this error was introduced by https://github.com/facebook/react-native/commit/3d05bac5872b42a5f412ec9042ce5be0b5d5b8d3.

## Changelog

<!-- Help reviewers and the release process by writing your own changelog entry. For an example, see:
https://reactnative.dev/contributing/changelogs-in-pull-requests
-->

[Internal] [Fixed] - Fix ReactAndroid not found in rn-tester build

Pull Request resolved: https://github.com/facebook/react-native/pull/35058

Test Plan:
- Clone the react-native repo.
- Run `cd packages/rn-tester`.
- Run `yarn install-android-jsc` and encounter the error.
- Apply the fix.
- Run `yarn install-android-jsc` and build succeeds.

Reviewed By: cipolleschi

Differential Revision: D40639856

Pulled By: cortinico

fbshipit-source-id: 58b51bcad0af7a21cac032c98484c5942a203e4c

build.gradle.kts
@@ -9,7 +9,9 @@ plugins { id("io.github.gradle-nexus.publish-plugin") version "1.1.0" }
 
 val reactAndroidProperties = java.util.Properties()
 
-File("./ReactAndroid/gradle.properties").inputStream().use { reactAndroidProperties.load(it) }
+File("$rootDir/ReactAndroid/gradle.properties").inputStream().use {
+  reactAndroidProperties.load(it)
+}
 
 version =
     if (project.hasProperty("isNightly") &&